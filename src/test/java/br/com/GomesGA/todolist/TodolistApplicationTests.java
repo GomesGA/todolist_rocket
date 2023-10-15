c1b8be962e781f601209408cd5c58c717a844e92
Guilherme Gomes <guilherme.gomess.alves@gmail.com>
2023-10-14 23:08:53 -0300

    Dockfiler deploy

---
+++ src/test/java/br/com/GomesGA/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.GomesGA.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
